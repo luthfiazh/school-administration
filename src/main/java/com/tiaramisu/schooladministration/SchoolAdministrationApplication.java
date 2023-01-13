757b3fb2f6e3e08799efc35274fc0b7abaf3e8c9
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-13 23:45:13 +0800

    (Chores) Main - Initial Spring Boot commit

---
+++ src/main/java/com/tiaramisu/schooladministration/SchoolAdministrationApplication.java
@@ -0,0 +1,13 @@
+package com.tiaramisu.schooladministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolAdministrationApplication.class, args);
+	}
+
+}
