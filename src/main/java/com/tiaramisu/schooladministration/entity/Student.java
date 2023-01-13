4744601ee651e54bc18933559dcc9e83820b9a0e
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-14 01:19:14 +0800

    DEV-STORY-01 - Creates initial changelog for students table, and its entity class

---
+++ src/main/java/com/tiaramisu/schooladministration/entity/Student.java
@@ -0,0 +1,31 @@
+package com.tiaramisu.schooladministration.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Data
+@Table(name = "students")
+@Entity
+@Builder
+@AllArgsConstructor
+public class Student {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(length = 36, nullable = false, columnDefinition = "VARCHAR(36)")
+    private String studentId;
+    @Column(length = 100, nullable = false)
+    private String email;
+    private String name;
+    private Date createdDate;
+    private Date modifiedDate;
+}
