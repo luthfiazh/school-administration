42d8cd2663caf9f779ba760293ad8ab76dae05b6
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-18 23:18:25 +0800

    DEV-STORY-02 - Adds @ComponentScan to detect Service components

--- src/main/java/com/tiaramisu/schooladministration/SchoolAdministrationApplication.java
+++ src/main/java/com/tiaramisu/schooladministration/SchoolAdministrationApplication.java
@@ -4,9 +4,11 @@ import com.tiaramisu.schooladministration.config.DataSourceConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @Import({DataSourceConfig.class})
+@ComponentScan(basePackages = {"com.tiaramisu.schooladministration.service"})
 @SpringBootApplication
 @EnableAutoConfiguration
 public class SchoolAdministrationApplication {
