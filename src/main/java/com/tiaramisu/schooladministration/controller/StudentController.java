f0b30aa767e74ecf1d66d419c1202d5cc6c7807f
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-19 01:05:18 +0800

    DEV-STORY-01 - Moves `/api/` prefix to context-path

--- src/main/java/com/tiaramisu/schooladministration/controller/StudentController.java
+++ src/main/java/com/tiaramisu/schooladministration/controller/StudentController.java
@@ -8,14 +8,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_STUDENT_INVALID_REQUEST_CODE;
 import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_STUDENT_SUCCESS_CODE;
 
 @RestController
-@RequestMapping("api")
 @RequiredArgsConstructor
 public class StudentController {
     private final StudentService studentService;
