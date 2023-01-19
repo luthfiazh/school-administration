6c4f3c1d1e3b9a3b05d877155a783e3792f1708d
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-19 23:40:10 +0800

    DEV-STORY-02 - Refactors default return in Controllers for adding Teacher & Student

--- src/main/java/com/tiaramisu/schooladministration/controller/StudentController.java
+++ src/main/java/com/tiaramisu/schooladministration/controller/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_USER_INVALID_REQUEST_CODE;
-import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_USER_SUCCESS_CODE;
+import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.GENERIC_ERROR_CODE;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,14 +21,14 @@ public class StudentController {
     @PostMapping(value = "/students", produces = "application/json", consumes = "application/json")
     public ResponseEntity<AddUserResponse> add(@RequestBody AddUserRequest addStudentRequest) {
         final AddUserResponse response = studentService.addStudent(addStudentRequest);
-        final boolean isRequestSuccessful = response.getResponseCode().equals(ADD_USER_SUCCESS_CODE);
-        if (isRequestSuccessful) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        }
         final boolean isRequestInvalid = response.getResponseCode().equals(ADD_USER_INVALID_REQUEST_CODE);
         if (isRequestInvalid) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        final boolean isMetWithError = response.getResponseCode().equals(GENERIC_ERROR_CODE);
+        if (isMetWithError) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
--- src/main/java/com/tiaramisu/schooladministration/controller/TeacherController.java
+++ src/main/java/com/tiaramisu/schooladministration/controller/TeacherController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_USER_INVALID_REQUEST_CODE;
-import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ADD_USER_SUCCESS_CODE;
+import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.GENERIC_ERROR_CODE;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,14 +21,14 @@ public class TeacherController {
     @PostMapping(value = "/teachers", produces = "application/json", consumes = "application/json")
     public ResponseEntity<AddUserResponse> add(@RequestBody AddUserRequest addTeacherRequest) {
         final AddUserResponse response = teacherService.addTeacher(addTeacherRequest);
-        final boolean isRequestSuccessful = response.getResponseCode().equals(ADD_USER_SUCCESS_CODE);
-        if (isRequestSuccessful) {
-            return ResponseEntity.status(HttpStatus.CREATED).body(response);
-        }
         final boolean isRequestInvalid = response.getResponseCode().equals(ADD_USER_INVALID_REQUEST_CODE);
         if (isRequestInvalid) {
             return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(response);
         }
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        final boolean isMetWithError = response.getResponseCode().equals(GENERIC_ERROR_CODE);
+        if (isMetWithError) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 }
