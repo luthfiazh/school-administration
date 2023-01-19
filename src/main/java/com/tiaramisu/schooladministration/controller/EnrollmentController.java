67aa7df522e28e496cd97c2c1580665ccf831c2b
Luthfi Azhari <luthfi.azhari@hotmail.com>
2023-01-19 12:51:47 +0800

    DEV-STORY-03 - Implements successful enrollment

--- src/main/java/com/tiaramisu/schooladministration/controller/EnrollmentController.java
+++ src/main/java/com/tiaramisu/schooladministration/controller/EnrollmentController.java
@@ -2,7 +2,9 @@ package com.tiaramisu.schooladministration.controller;
 
 import com.tiaramisu.schooladministration.model.EnrollmentRequest;
 import com.tiaramisu.schooladministration.model.EnrollmentResponse;
+import com.tiaramisu.schooladministration.service.EnrollmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class EnrollmentController {
+    private final EnrollmentService enrollmentService;
+
     @PostMapping(value = "/register", produces = "application/json", consumes = "application/json")
     public ResponseEntity<EnrollmentResponse> register(@RequestBody EnrollmentRequest enrollmentRequest) {
-        return null;
+        EnrollmentResponse enrollmentResponse = enrollmentService.enrollStudent(enrollmentRequest);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(enrollmentResponse);
     }
 }
--- src/test/java/com/tiaramisu/schooladministration/controller/EnrollmentControllerTest.java
+++ src/test/java/com/tiaramisu/schooladministration/controller/EnrollmentControllerTest.java
@@ -2,13 +2,22 @@ package com.tiaramisu.schooladministration.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tiaramisu.schooladministration.model.EnrollmentRequest;
+import com.tiaramisu.schooladministration.model.EnrollmentResponse;
+import com.tiaramisu.schooladministration.service.impl.EnrollmentServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
+import static com.tiaramisu.schooladministration.utility.Constant.ResponseCode.ENROLLMENT_SUCCESS_CODE;
+import static com.tiaramisu.schooladministration.utility.Constant.ResponseMessage.ENROLLMENT_SUCCESS_MESSAGE;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,15 +28,28 @@ class EnrollmentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    void register_shouldReturnHttpOk_whenEndpointIsHit() throws Exception {
-        EnrollmentRequest request = EnrollmentRequest.builder().build();
-        final String requestInJson = new ObjectMapper().writeValueAsString(request);
+    @MockBean
+    private EnrollmentServiceImpl enrollmentService;
 
-        mockMvc.perform(post(ENDPOINT_URI)
+    @Test
+    void register_shouldReturnHttpNoContent_whenEndpointIsHitGivenAppropriateRequest() throws Exception {
+        final EnrollmentRequest request = EnrollmentRequest.builder()
+                .build();
+        final String requestInJson = new ObjectMapper().writeValueAsString(request);
+        final EnrollmentResponse response = EnrollmentResponse.builder()
+                .responseCode(ENROLLMENT_SUCCESS_CODE)
+                .responseMessage(ENROLLMENT_SUCCESS_MESSAGE)
+                .build();
+        final String responseInJson = new ObjectMapper().writeValueAsString(response);
+        when(enrollmentService.enrollStudent(request)).thenReturn(response);
+
+        MvcResult result = mockMvc.perform(post(ENDPOINT_URI)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestInJson))
-                .andExpect(status().isOk())
+                .andExpect(status().isNoContent())
                 .andReturn();
+
+        verify(enrollmentService).enrollStudent(request);
+        assertEquals(responseInJson, result.getResponse().getContentAsString());
     }
 }
